169738537a2766a94058e8361696c535f1854145
xuebaitv <jiangzhiwen59@outlook.com>
2025-06-04 18:10:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    url: 'https://video.xuebaitv.xyz',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
